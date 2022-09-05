c5b4051216f38935be9d376096799327b8baca82
relliikz <100636896+relliikz@users.noreply.github.com>
2022-09-05 18:03:12 +1000

    1 file modified, 1 added

--- models/Cookie.ts
+++ models/Cookie.ts
@@ -1,13 +1,22 @@
+import { Colours } from './Colours.js';
+
 export class Cookie {
   //TODO - Cookies have three attributes; name, colour, chocolateChipNum
   // colour is a string
   // There is one constructor which takes a name as a parameter.  Initialise colour to brown and chocolateChipNum to 0
   name: string;
-  colour: string;
+  colour: Colours;
   chocolateChipNum: number;
-  constructor(name: string, colour: string, chocolateChipNum: number) {
+  sprinkleColour: Colours;
+  constructor(
+    name: string,
+    colour: Colours,
+    chocolateChipNum: number,
+    sprinkleColour: Colours
+  ) {
     this.name = name;
-    this.colour = 'brown';
+    this.colour = colour;
     this.chocolateChipNum = 0;
+    this.sprinkleColour = sprinkleColour;
   }
 }
